a4a909d991f944d0a81af57f2e78c467140e2db2
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-04-17 20:24:29 +0300

    Adding middleware

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,5 +1,19 @@
 const userLogin = async(req,res)=>{
-    console.log("User Login Functionality")
+    const result = "This is the query here"
+    console.log("User Login Functionality");
+    let {user_id,email} = req.body;
+
+    const token = jwt.sign(
+        { user_id: user_id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+    result.token = token;
+    
+    res.status(201).json(result);
 }
 
 const addSubUser = async(req,res)=>{
--- routes/Users.js
+++ routes/Users.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {userLogin, addSubUser} = require('../controllers/Users.js');
 
+//importing middlewares
+const auth = require("./middleware/auth");
+
 router.get('/login',userLogin)
-router.get('/add-sub-user',addSubUser)
+router.get('/add-sub-user',auth,addSubUser)
 
 module.exports = router;
